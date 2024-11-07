04dc1476af6165243d8e9b719c37d6ae6ed0bc36
Svetlana Kapelko <56638107+Svkapelko@users.noreply.github.com>
2024-11-07 11:04:15 +0300

    Добавила файл claire_math_package

---
+++ index.js
@@ -0,0 +1,17 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // Максимум не включается, минимум включается
+  }
+
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // Максимум и минимум включаются
+  }
+
+  module.exports = { getRandomInt, getRandomIntInclusive };
